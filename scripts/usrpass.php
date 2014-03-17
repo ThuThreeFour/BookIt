4e8825fe353980b36bc7ca4634cac782881805ad
ThuThreeFour <thu.tran10@yahoo.com>
2014-03-17 18:16:40 -0400

    worked on userlogin

---
+++ scripts/usrpass.php
@@ -0,0 +1,13 @@
+<?php
+  # File: usrpass.php
+  # Thu A.Tran, UMass Lowell Computer Science, thu_tran2@student.uml.edu
+  # Updated on February 06, 2014
+  # (Documentation is a modification of Professor Jesse Heines's work.)
+
+  # Code below used from PHP & MySQL:  The Missing Manual by Brett McLaughlin  
+  # Ch.5 pg.136 
+  define("DATABASE_USERNAME", "ttran");
+  define("DATABASE_PASSWORD", "tt6563");
+?>
+
+
